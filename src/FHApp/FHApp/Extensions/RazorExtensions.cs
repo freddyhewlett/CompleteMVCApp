9a0841230f8d12e442ee65311a72147e577fa478
Federico <freddyhewlett@gmail.com>
2023-05-24 21:40:25 -0300

    views + partial views

---
+++ src/FHApp/FHApp/Extensions/RazorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace FH.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string DocumentFormat(this RazorPage page, int devType, string document)
+        {
+            return devType == 1 ? Convert.ToInt64(document).ToString(@"000\.000\.000\-00") : Convert.ToInt64(document).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
